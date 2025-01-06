93633415ac1cc965f187bffc279f8a48f879834d
ROUNAK HARUGIRE <150342234+Rounak-Harugire@users.noreply.github.com>
2025-01-06 22:27:09 +0530

    Add files via upload

---
+++ level_1 & task_1.py
@@ -0,0 +1,7 @@
+def reverse_string(input_string):
+   
+    return input_string[::-1]
+
+input_str = input("Enter a string :- ")
+reversed_str = reverse_string(input_str)
+print("Reversed of string is :-", reversed_str)
